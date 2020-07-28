dad60cc36970cdbc259a8886de33c76c81eb6412
Gilbert <gilbertduenas@users.noreply.github.com>
2020-07-29 07:55:26 +0900

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,22 +1,29 @@
 /* hackerrank - sql */
 -- ----------------------------------------------------------------------------------
-/* https://www.hackerrank.com/challenges/select-by-id/problem */
-SELECT * FROM city WHERE id = 1661
 
 /* https://www.hackerrank.com/challenges/select-all-sql/problem */
 SELECT * FROM city
 
-/* https://www.hackerrank.com/challenges/revising-the-select-query-2/problem */
-SELECT name FROM city WHERE countrycode = 'USA' AND population > 120000
+/* https://www.hackerrank.com/challenges/select-by-id/problem */
+SELECT * FROM city WHERE id = 1661
+
+-- ----------------------------------------------------------------------------------
 
 /* https://www.hackerrank.com/challenges/revising-the-select-query/problem */
 SELECT * FROM city WHERE countrycode = 'USA' AND population > 100000
+
+/* https://www.hackerrank.com/challenges/revising-the-select-query-2/problem */
+SELECT name FROM city WHERE countrycode = 'USA' AND population > 120000
+
+-- ----------------------------------------------------------------------------------
 
 /* https://www.hackerrank.com/challenges/japanese-cities-attributes/problem */
 SELECT * FROM city WHERE countrycode = 'JPN'
 
 /* https://www.hackerrank.com/challenges/japanese-cities-name/problem */
 SELECT name FROM city WHERE countrycode = 'JPN'
+
+-- ----------------------------------------------------------------------------------
 
 /* https://www.hackerrank.com/challenges/weather-observation-station-1/problem */
 SELECT city, state FROM station
@@ -26,3 +33,6 @@ SELECT ROUND(SUM(lat_n), 2), ROUND(SUM(long_w), 2) FROM station
 
 /* https://www.hackerrank.com/challenges/weather-observation-station-3/problem */
 SELECT DISTINCT city FROM station WHERE id%2 LIKE 0
+
+/* https://www.hackerrank.com/challenges/weather-observation-station-4/problem */
+
