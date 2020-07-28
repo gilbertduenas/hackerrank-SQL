ed79b5756d4f9d25e9bf5a398bbf135bf73c81f3
Gilbert <gilbertduenas@users.noreply.github.com>
2020-07-29 08:09:25 +0900

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,5 +1,5 @@
 /* hackerrank - sql */
--- ----------------------------------------------------------------------------------
+/* ------------------------------------------------------------------------------- */
 
 /* https://www.hackerrank.com/challenges/select-all-sql/problem */
 SELECT * FROM city
@@ -7,7 +7,7 @@ SELECT * FROM city
 /* https://www.hackerrank.com/challenges/select-by-id/problem */
 SELECT * FROM city WHERE id = 1661
 
--- ----------------------------------------------------------------------------------
+/* ------------------------------------------------------------------------------- */
 
 /* https://www.hackerrank.com/challenges/revising-the-select-query/problem */
 SELECT * FROM city WHERE countrycode = 'USA' AND population > 100000
@@ -15,7 +15,7 @@ SELECT * FROM city WHERE countrycode = 'USA' AND population > 100000
 /* https://www.hackerrank.com/challenges/revising-the-select-query-2/problem */
 SELECT name FROM city WHERE countrycode = 'USA' AND population > 120000
 
--- ----------------------------------------------------------------------------------
+/* ------------------------------------------------------------------------------- */
 
 /* https://www.hackerrank.com/challenges/japanese-cities-attributes/problem */
 SELECT * FROM city WHERE countrycode = 'JPN'
@@ -23,7 +23,7 @@ SELECT * FROM city WHERE countrycode = 'JPN'
 /* https://www.hackerrank.com/challenges/japanese-cities-name/problem */
 SELECT name FROM city WHERE countrycode = 'JPN'
 
--- ----------------------------------------------------------------------------------
+/* ------------------------------------------------------------------------------- */
 
 /* https://www.hackerrank.com/challenges/weather-observation-station-1/problem */
 SELECT city, state FROM station
@@ -36,3 +36,8 @@ SELECT DISTINCT city FROM station WHERE id%2 LIKE 0
 
 /* https://www.hackerrank.com/challenges/weather-observation-station-4/problem */
 
+/* ------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------------- */
