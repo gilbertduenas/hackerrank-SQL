474bfc64299bac89c2ec35a56d6d60dfc3e9cadd
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-04 07:22:54 +0900

    Update advanced_select.sql

--- advanced_select.sql
+++ advanced_select.sql
@@ -1,7 +1,17 @@
 /* ------------------------------------------------------------------------------- */
 
 /* https://www.hackerrank.com/challenges/what-type-of-triangle/problem */
-
+SELECT 
+CASE 
+    WHEN A+B>C AND A+C>B AND B+C>A THEN
+        CASE
+            WHEN A=C AND B=C THEN 'Equilateral'
+            WHEN A=B OR A=C OR B=C THEN 'Isosceles'
+            ELSE 'Scalene'
+        END
+    ELSE 'Not A Triangle'
+END
+FROM  triangles;
 
 /* ------------------------------------------------------------------------------- */
 /* ------------------------------------------------------------------------------- */
