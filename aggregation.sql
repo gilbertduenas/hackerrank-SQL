869384b00839017a6868ef123314cacff1aa58cb
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-05 11:15:52 +0900

    Create aggregation.sql

---
+++ aggregation.sql
@@ -0,0 +1,10 @@
+/* ------------------------------------------------------------------------------- */
+
+/* https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem */
+SELECT COUNT(name) FROM city WHERE population > 100000;
+
+/* https://www.hackerrank.com/challenges/revising-aggregations-sum/problem */
+SELECT SUM(population) FROM city WHERE district = 'California';
+
+/* https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem */
+SELECT AVG(population) FROM city WHERE district = 'California';
