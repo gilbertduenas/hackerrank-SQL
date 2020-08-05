eae61d2b3b6c888927974929dac6f7eb1478c712
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-05 18:47:35 +0900

    Update aggregation.sql
    Alphabetizing URLs.

--- aggregation.sql
+++ aggregation.sql
@@ -3,11 +3,12 @@
 /* https://www.hackerrank.com/challenges/average-population/problem */
 SELECT FLOOR(AVG(population)) FROM city;
 
-/* https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem */
-SELECT COUNT(name) FROM city WHERE population > 100000;
-
 /* https://www.hackerrank.com/challenges/revising-aggregations-sum/problem */
 SELECT SUM(population) FROM city WHERE district = 'California';
 
 /* https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem */
 SELECT AVG(population) FROM city WHERE district = 'California';
+
+/* https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem */
+SELECT COUNT(name) FROM city WHERE population > 100000;
+
