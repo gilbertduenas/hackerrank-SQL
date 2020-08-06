9f095b683afaca8ef56db94c26fd5918b934306d
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-06 09:54:50 +0900

    Update aggregation.sql

--- aggregation.sql
+++ aggregation.sql
@@ -6,6 +6,9 @@ SELECT FLOOR(AVG(population)) FROM city;
 /* https://www.hackerrank.com/challenges/japan-population/problem */
 SELECT SUM(population) FROM city WHERE countrycode = 'JPN';
 
+/* https://www.hackerrank.com/challenges/population-density-difference/problem */
+SELECT MAX(population) - MIN(population) FROM city;
+
 /* https://www.hackerrank.com/challenges/revising-aggregations-sum/problem */
 SELECT SUM(population) FROM city WHERE district = 'California';
 
@@ -14,3 +17,4 @@ SELECT AVG(population) FROM city WHERE district = 'California';
 
 /* https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem */
 SELECT COUNT(name) FROM city WHERE population > 100000;
+
