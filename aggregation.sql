10ba0a9da0c892c4f64ea5d24c35053b2b726d4b
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-06 19:31:40 +0900

    Update aggregation.sql

--- aggregation.sql
+++ aggregation.sql
@@ -3,6 +3,9 @@
 /* https://www.hackerrank.com/challenges/average-population/problem */
 SELECT FLOOR(AVG(population)) FROM city;
 
+/* https://www.hackerrank.com/challenges/earnings-of-employees/problem */
+SELECT months * salary AS earnings, COUNT(*) FROM employee GROUP BY earnings ORDER BY earnings DESC LIMIT 1;
+
 /* https://www.hackerrank.com/challenges/japan-population/problem */
 SELECT SUM(population) FROM city WHERE countrycode = 'JPN';
 
@@ -19,3 +22,7 @@ SELECT AVG(population) FROM city WHERE district = 'California';
 SELECT COUNT(name) FROM city WHERE population > 100000;
 
 /* https://www.hackerrank.com/challenges/the-blunder/problem */
+SELECT CEIL(AVG(salary) - AVG(REPLACE(salary, '0', ''))) FROM employees;
+
+
+
