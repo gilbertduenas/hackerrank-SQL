46917f43cdbc40e85e99e0d29db6abefae327ed0
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-06 21:47:54 +0900

    Update aggregation.sql

--- aggregation.sql
+++ aggregation.sql
@@ -12,6 +12,8 @@ SELECT SUM(population) FROM city WHERE countrycode = 'JPN';
 /* https://www.hackerrank.com/challenges/population-density-difference/problem */
 SELECT MAX(population) - MIN(population) FROM city;
 
+/* ------------------------------------------------------------------------------- */
+
 /* https://www.hackerrank.com/challenges/revising-aggregations-sum/problem */
 SELECT SUM(population) FROM city WHERE district = 'California';
 
@@ -21,8 +23,22 @@ SELECT AVG(population) FROM city WHERE district = 'California';
 /* https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem */
 SELECT COUNT(name) FROM city WHERE population > 100000;
 
+/* ------------------------------------------------------------------------------- */
+
 /* https://www.hackerrank.com/challenges/the-blunder/problem */
 SELECT CEIL(AVG(salary) - AVG(REPLACE(salary, '0', ''))) FROM employees;
 
+/* ------------------------------------------------------------------------------- */
 
+/* https://www.hackerrank.com/challenges/weather-observation-station-13/problem */
+SELECT ROUND(SUM(lat_n), 4) FROM station WHERE lat_n > 38.7880 AND lat_n < 137.2345;
+
+/* https://www.hackerrank.com/challenges/weather-observation-station-14/problem */
+SELECT ROUND(MAX(lat_n), 4) FROM station WHERE lat_n < 137.2345;
+
+/* https://www.hackerrank.com/challenges/weather-observation-station-15/problem */
+SELECT ROUND(long_w, 4) FROM station WHERE lat_n < 137.2345 ORDER BY lat_n DESC LIMIT 1;
+
+/* https://www.hackerrank.com/challenges/weather-observation-station-16/problem */
+SELECT ROUND(lat_n, 4) FROM station WHERE lat_n > 38.7780 ORDER BY lat_n LIMIT 1;
 
