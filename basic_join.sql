b4c3c1f47d4900ca6a2d5c59c588e0574b9db87d
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-08 08:27:45 +0900

    Update basic_join.sql

--- basic_join.sql
+++ basic_join.sql
@@ -1,6 +1,19 @@
 /* ---------------------------------------------------------------------------------------------------------------------- */
 
+/* https://www.hackerrank.com/challenges/african-cities/problem */
+SELECT city.name FROM city 
+INNER JOIN country ON city.countrycode = country.code
+WHERE country.continent = 'Africa';
+
 /* https://www.hackerrank.com/challenges/asian-population/problem */
+SELECT SUM(city.population) FROM city 
+INNER JOIN country ON city.countrycode = country.code
+WHERE country.continent = 'Asia';
+
+/* https://www.hackerrank.com/challenges/average-population-of-each-continent/problem */
+SELECT country.continent, FLOOR(AVG(city.population)) FROM city 
+INNER JOIN country ON city.countrycode = country.code
+GROUP BY country.continent;
 
 
 /* ---------------------------------------------------------------------------------------------------------------------- */
