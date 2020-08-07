3b84bca2893aaf9ef6f26e74c303dc8a993fd127
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-08 08:49:39 +0900

    Update basic_join.sql

--- basic_join.sql
+++ basic_join.sql
@@ -26,6 +26,9 @@ INNER JOIN grades
 ON students.marks BETWEEN grades.min_mark AND grades.max_mark
 ORDER BY grades.grade DESC, students.name, students.marks;
 
+/* https://www.hackerrank.com/challenges/full-score/problem */
+
+
 /* ---------------------------------------------------------------------------------------------------------------------- */
 
 /* ---------------------------------------------------------------------------------------------------------------------- */
